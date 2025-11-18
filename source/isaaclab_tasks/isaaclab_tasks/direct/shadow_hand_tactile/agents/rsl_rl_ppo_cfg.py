6000c1c2770c9379a115cf2cce387d4dceb70813
RyotaTanaka-737 <ryota.tanaka.703@ctc-g.co.jp>
2025-11-19 02:43:19 +0900

    ShadowHandTactileFFPPORunnerCfgクラスを追加し、PPOアルゴリズムの設定を更新

--- source/isaaclab_tasks/isaaclab_tasks/direct/shadow_hand_tactile/agents/rsl_rl_ppo_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/direct/shadow_hand_tactile/agents/rsl_rl_ppo_cfg.py
@@ -96,3 +96,33 @@ class ShadowHandVisionFFPPORunnerCfg(RslRlOnPolicyRunnerCfg):
         desired_kl=0.01,
         max_grad_norm=1.0,
     )
+
+@configclass
+class ShadowHandTactileFFPPORunnerCfg(RslRlOnPolicyRunnerCfg):
+    num_steps_per_env = 64
+    max_iterations = 50000
+    save_interval = 250
+    experiment_name = "shadow_hand_tactile"
+    policy = RslRlPpoActorCriticCfg(
+        init_noise_std=1.0,
+        actor_obs_normalization=True,
+        critic_obs_normalization=True,
+        input_dims_list = [48, 65, 19, 25, 128],
+        actor_hidden_dims=[1024, 512, 512, 256, 128],
+        critic_hidden_dims=[1024, 512, 512, 256, 128],
+        activation="elu",
+    )
+    algorithm = RslRlPpoAlgorithmCfg(
+        value_loss_coef=1.0,
+        use_clipped_value_loss=True,
+        clip_param=0.2,
+        entropy_coef=0.005,
+        num_learning_epochs=5,
+        num_mini_batches=4,
+        learning_rate=5.0e-4,
+        schedule="adaptive",
+        gamma=0.99,
+        lam=0.95,
+        desired_kl=0.01,
+        max_grad_norm=1.0,
+    )
